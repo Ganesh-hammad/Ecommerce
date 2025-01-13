7b67e50fc39ca04ad029463bbc196cc731420bd2
Ganesh Hammad <ganeshhammad837@gmail.com>
2025-01-14 00:02:28 +0530

    create a cart component funtionality

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title';
+import { assets } from '../assets/frontend_assets/assets';
 
 const Cart = () => {
+  const {products,currency, cartItems, updateQuantity} = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
+  useEffect(()=>{
+   let temData = [];
+   for(const items in cartItems){
+    for(const item in cartItems[items]){
+      if(cartItems[items][item]>0){
+         temData.push({
+          _id: items,
+          size:item,
+          quatity:cartItems[items][item]
+         })
+      }
+    }
+  }
+   if(temData.size !== ""){
+     setCartData(temData);
+   }
+  }, [cartItems])
   return (
-    <div>Cart</div>
+    <div className='border-t pt-14'>
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR"} text2={"CART"}/>
+      </div>
+      <div className="">
+        {
+          cartData.map((item, index)=>{
+          const productData = products.find((prodcut)=> prodcut._id === item._id);
+          return(
+            <div key={index} className="py-4 border-b border-t text-gray-700 grid grid-clos-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4 ">
+              <div className="flex items-start gap-6">
+                <img src={productData.image[0]} alt="" className="w-16 sm:w-20" />
+              <div>
+              <p className="text-xs sm:text-lg font-medium ">{productData.name}</p>
+              <div className="flex items-center gap-5 mt-2">
+                <p className="">{currency}{productData.price}</p>
+                <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50 ">{item.size} </p>
+              </div>
+              </div>
+            </div>
+             <input onChange={(e) => e.target.value === '' || e.target.value === 0 ? null : updateQuantity(item._id, item.size, Number(e.target.value))} type="number" className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' min={1} defaultValue={item.quatity} />
+             <img onClick={()=>updateQuantity(item._id, item.size, 0) } className='w-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
+             </div>
+          )
+          })
+        }
+      </div>
+    </div>
   )
 }
 
